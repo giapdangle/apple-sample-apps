3dc2f2ec3e9963c719179b1058f1b7a1a92183e1
Marcos Sánchez-Dehesa <san.dehesa@gmail.com>
2014-08-12 10:41:07 +0200

    Workspace and two first sample app creation

---
+++ HelloRelayr/HelloRelayr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloRelayr
+//
+//  Created by Marcos Sánchez-Dehesa on 12/08/14.
+//  Copyright (c) 2014 Jamie Banford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
