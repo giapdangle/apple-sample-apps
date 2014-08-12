69e790c3700a5432d5921246d9ee2df1bbe4092e
Jamie Banford <jamie@relayr.de>
2014-08-12 12:21:15 +0200

    Clean up boilerplate view controller and rename

---
+++ HelloRelayr/iOSApp/Classes/SignInViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
---
+++ HelloRelayr/iOSApp/Classes/SignInViewController.m
@@ -0,0 +1,19 @@
+#import "SignInViewController.h"
+
+@interface SignInViewController ()
+
+@end
+
+@implementation SignInViewController
+            
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+}
+
+@end
