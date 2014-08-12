d870d0b95b5f45b870b77327779deb0010ce0b30
Marcos Sánchez-Dehesa <san.dehesa@gmail.com>
2014-08-12 11:52:22 +0200

    Hello Wunderbar structure

---
+++ HelloWunderbar/iOSApp/Classes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS App
+//
+//  Created by Marcos Sánchez-Dehesa on 12/08/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
