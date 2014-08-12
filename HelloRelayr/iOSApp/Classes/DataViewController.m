b7dfedaccf07af52fca1abe1d7903e50dcfbaeca
Jamie Banford <jamie@relayr.de>
2014-08-12 17:24:13 +0200

    Implement sign in and sign out; delete superfluous SignInViewController class

--- HelloRelayr/iOSApp/Classes/DataViewController.m
+++ HelloRelayr/iOSApp/Classes/DataViewController.m
@@ -1,4 +1,10 @@
 #import "DataViewController.h"
+#import "Relayr/RLARemoteUser.h"
+
+static NSString* const kHelloRelayrRedirectURI = @"https://relayr.io";
+static NSString* const kHelloRelayrAppID = @"e2155c3e-aaa4-4706-99e2-ea03bdbf9c16";
+static NSString* const kHelloRelayrClientID = @"s2toGeu.OPkBHfMupFu6YjT9cs4upoyZ";
+static NSString* const kHelloRelayrSecret = @"79nl.Lbjz7pY1pGad_I1uIPwjki2dEYY";
 
 @interface DataViewController ()
 
@@ -6,9 +12,44 @@
 
 @implementation DataViewController
 
+- (void)viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  
+  [RLARemoteUser authenticateUserWithClientID:kHelloRelayrClientID
+                                        appID:kHelloRelayrAppID
+                                    appSecret:kHelloRelayrSecret
+                                  redirectURI:kHelloRelayrRedirectURI
+                     presentingViewController:self
+                            completionHandler:^(RLARemoteUser *user, NSError *error) {
+     
+     if (error) {
+       // Authentication failed
+       NSLog(@"%@", error);
+     } else {
+       // A user exists
+       NSLog(@"%@", user);
+     }
+   }];
+}
+
 - (void)viewDidLoad
 {
   [super viewDidLoad];
+}
+
+- (IBAction)signOut:(id)sender
+{
+  [RLARemoteUser authenticateUserWithClientID:kHelloRelayrClientID
+                                        appID:kHelloRelayrAppID
+                                    appSecret:kHelloRelayrSecret
+                                  redirectURI:kHelloRelayrRedirectURI
+                     presentingViewController:self
+                            completionHandler:^(RLARemoteUser *user, NSError *error) {
+                              
+                              [user unauthenticateUser];
+                            }];
+  [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 @end
