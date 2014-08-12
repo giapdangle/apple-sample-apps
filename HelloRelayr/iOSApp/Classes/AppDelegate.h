7e76b7073160c3f9f7cae1ff39ff732b8ab3f5a1
Jamie Banford <jamie@relayr.de>
2014-08-12 12:06:44 +0200

    Clean up boilerplate code

--- HelloRelayr/iOSApp/Classes/AppDelegate.h
+++ HelloRelayr/iOSApp/Classes/AppDelegate.h
@@ -1,17 +1,7 @@
-//
-//  AppDelegate.h
-//  iOSApp
-//
-//  Created by Jamie on 12/08/2014.
-//
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-
 @end
-
--- HelloRelayr/iOSApp/Classes/main.m
+++ HelloRelayr/iOSApp/Classes/main.m
@@ -1,16 +1,13 @@
-//
-//  main.m
-//  iOSApp
-//
-//  Created by Jamie on 12/08/2014.
-//
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-  @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+int main(int argc, char * argv[])
+{
+  @autoreleasepool
+  {
+    return UIApplicationMain(argc,
+                             argv,
+                             nil,
+                             NSStringFromClass([AppDelegate class]));
   }
 }
